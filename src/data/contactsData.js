1d997d8cd0804954b941f8b597c07ba3fd170b48
bingo526 <chief.victoru@gmail.com>
2023-12-10 16:46:46 -0800

    update phone number

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
   email: "chief.victoru@gmail.com",
-  phone: "+12134440332",
+  phone: "+18059740527",
   address: "2224 Benito Drive 93010 Camarillo CA",
   calendly: "https://cal.com/victor-ruiz/30min",
   sheetAPI: "https://sheetdb.io/api/v1/vm0tv35pedksw",
